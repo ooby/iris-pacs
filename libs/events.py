446059c90b0a1da11282ecd3404b34745903d730
Afanasy Argunov <afanasy@argunov.com>
2020-11-05 19:29:34 +0900

    RMQ added

--- libs/events.py
+++ libs/events.py
@@ -1,101 +1,56 @@
+import datetime
 import os
+import uuid
 from pydicom import dcmread
 from pydicom.dataset import Dataset
 from pydicom.filewriter import write_file_meta_info
 from .io import get_studies
 
 
-def handle_store(event, path, LOGGER, code):
-    # GUID = ''  # TODO: Add GUID from pymongo table new record with data check
-    if event.dataset.SeriesInstanceUID:
-        path = os.path.join(path, event.dataset.StudyInstanceUID, event.dataset.SeriesInstanceUID)
+def handle_store(event, LOGGER, db, code):
+    studies = db['studies']
+    study_record = studies.find_one({'assoc': event.assoc.name})
+    path = study_record['path']
+    if event.dataset.StudyDescription:
+        path = os.path.join(path, event.dataset.StudyDescription,
+                            event.dataset.SeriesDescription)
     else:
-        path = os.path.join(path, event.dataset.StudyInstanceUID)
+        path = os.path.join(path, event.dataset.StudyDescription)
     try:
         os.makedirs(path, exist_ok=True)
     except:
         return 0xC001
-    fname = os.path.join(path, event.request.AffectedSOPInstanceUID)
+    fname = os.path.join(path, str(event.message_id) + '.dcm')
     with open(fname, 'wb') as f:
         f.write(b'\x00' * 128)
         f.write(b'DICM')
         write_file_meta_info(f, event.file_meta)
         f.write(event.request.DataSet.getvalue())
     LOGGER.info(f'Written data in { fname }')
-    LOGGER.info(f'C-STORE for {event.request.AffectedSOPInstanceUID}')
+    LOGGER.info(f'C-STORE for {event.message_id}')
     return 0x0000
 
 
-def handle_open(event, LOGGER):
+def handle_open(event, path, LOGGER, db, code):
+    guid = str(uuid.uuid4())
+    study_record = {'studyUid': guid,
+                    'assoc': event.assoc.name,
+                    'mo_code': code,
+                    'path': os.path.join(path, guid),
+                    'created': datetime.datetime.utcnow()}
+    studies = db['studies']
+    studies.insert_one(study_record)
     LOGGER.info(f'Connected with remote at { event.address }')
 
 
 def handle_find(event, LOGGER):
-    """Handle a C-FIND request event."""
-    ds = event.identifier
-
-    instances = get_studies(stop_before_pixels=True)
-
-    if 'QueryRetrieveLevel' not in ds:
-        yield 0xC000, None
-        return
-
-    # matching = []
-    # if ds.QueryRetrieveLevel == 'STUDY':
-    #     if 'StudyID' in ds:
-    #         matching.append()
-
-    # if ds.QueryRetrieveLevel == 'PATIENT':
-    #     if 'PatientName' in ds:
-    #         if ds.PatientName not in ['*', '', '?']:
-    #             matching = [
-    #                 inst for inst in instances if inst.PatientName == ds.PatientName]
-
-    for instance in instances:
-        # Check if C-CANCEL has been received
-        if event.is_cancelled:
-            yield (0xFE00, None)
-            return
-        # identifier = Dataset()
-        # identifier.StudyID = instance.StudyID
-        # identifier.PatientName = instance.PatientName
-        # identifier.PatientID = instance.PatientID
-        # identifier.StudyDate = instance.StudyDate
-        # identifier.StudyTime = instance.StudyTime
-        # identifier.QueryRetrieveLevel = ds.QueryRetrieveLevel
-        yield (0xFF00, instance)
+    LOGGER.info(f'Handle a C-FIND request event')
+    return 0x0000
 
 
 def handle_get(event, LOGGER):
-    """Handle a C-GET request event."""
-    ds = event.identifier
-    if 'QueryRetrieveLevel' not in ds:
-        # Failure
-        yield 0xC000, None
-        return
-
-    instances = get_studies(stop_before_pixels=True)
-
-    matching = []
-    if ds.QueryRetrieveLevel == 'PATIENT':
-        if 'PatientID' in ds:
-            matching = [
-                inst for inst in instances if inst.PatientID == ds.PatientID]
-
-    if ds.QueryRetrieveLevel == 'STUDY':
-        if 'StudyInstanceUID' in ds:
-            matching = [
-                inst for inst in instances if inst.StudyInstanceUID == ds.StudyInstanceUID]
-
-    yield len(instances)
-
-    for instance in matching:
-        # Check if C-CANCEL has been received
-        if event.is_cancelled:
-            yield (0xFE00, None)
-            return
-        # Pending
-        yield (0xFF00, instance)
+    LOGGER.info(f'Handle a C-GET request event')
+    return 0x0000
 
 
 def handle_echo(event, LOGGER):
@@ -103,5 +58,12 @@ def handle_echo(event, LOGGER):
     return 0x0000
 
 
-def handle_close(event, LOGGER, code):
+def handle_close(event, LOGGER, db, code, channel):
+    studies = db['studies']
+    study_record = studies.find_one({'assoc': event.assoc.name})
+    queue = 'processing'
+    message = study_record['studyUid']
+    channel.queue_declare(queue=queue)
+    channel.basic_publish(exchange='', routing_key=queue, body=message)
+    print(f'[x] Sent {message} for {queue} queue')
     LOGGER.info(f'Connection closed with remote at { event.address }')
--- start.py
+++ start.py
@@ -2,6 +2,10 @@ import asyncio
 import logging
 import os
 import sys
+import pika
+import pymongo
+
+from bson.objectid import ObjectId
 
 from libs.events import handle_close, handle_open, handle_store, handle_echo, handle_find, handle_get
 from pynetdicom import (
@@ -15,18 +19,20 @@ from pynetdicom.sop_class import (
 
 
 def main():
-    if len(sys.argv) < 3:
-        print(f'Not enough arguments. python3 start.py ADDRESS PORT')
+    if len(sys.argv) < 6:
+        print(f'Not enough arguments. python3 start.py ADDRESS PORT MQ_HOST DB_ADDRESS DB_PORT')
     else:
         address = sys.argv[1]
         port = int(sys.argv[2])
+        MQ_HOST = sys.argv[3]
+        DB_ADDRESS = sys.argv[4]
+        DB_PORT = int(sys.argv[5])
         with open('/code', 'r') as fp:
             line = fp.readline()
             line = line.rstrip('\n')
             MO_CODE = line
         logging.basicConfig(level=logging.DEBUG)
         LOGGER = logging.getLogger('pynetdicom')
-
         ae = AE()
         ae.ae_title = b'IRIS-PACS'
         for cx in ae.supported_contexts:
@@ -43,17 +49,31 @@ def main():
         storage_sop_classes.append('1.2.840.10008.5.1.4.1.2.2.3')
         for uid in storage_sop_classes:
             ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)
+        client = pymongo.MongoClient(DB_ADDRESS, DB_PORT)
+        db = client['iris-pacs']
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=MQ_HOST))
+        MQ_CHANNEL = connection.channel()
         handlers = [
-            (evt.EVT_CONN_OPEN, handle_open, [LOGGER]),
-            (evt.EVT_C_STORE, handle_store, ['/data/scans', LOGGER, MO_CODE]),
+            (evt.EVT_CONN_OPEN, handle_open, [
+             '/data/scans', LOGGER, db, MO_CODE]),
+            (evt.EVT_C_STORE, handle_store, [LOGGER, db, MO_CODE]),
             (evt.EVT_C_ECHO, handle_echo, [LOGGER]),
             (evt.EVT_C_FIND, handle_find, [LOGGER]),
             (evt.EVT_C_GET, handle_get, [LOGGER]),
-            (evt.EVT_CONN_CLOSE, handle_close, [LOGGER, MO_CODE])
+            (evt.EVT_CONN_CLOSE, handle_close,
+             [LOGGER, db, MO_CODE, MQ_CHANNEL])
         ]
         print(f'Starting Store SCU at {port} port for {MO_CODE}')
         ae.start_server((address, port), evt_handlers=handlers)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
