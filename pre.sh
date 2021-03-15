ecd3da1053b35c30859194b62e1d0111a19bd1b7
Afanasy Argunov <afanasy@argunov.com>
2021-03-15 14:01:41 +0900

    rabbitmq version on pre.sh script

--- pre.sh
+++ pre.sh
@@ -2,4 +2,4 @@
 docker pull mongo
 docker pull rabbitmq:3.8.9
 docker run --restart always -dit -p 27017:27017 --name iris-db mongo
-docker run --restart always -dit -p 5672:5672 --name iris-mq rabbitmq
+docker run --restart always -dit -p 5672:5672 --name iris-mq rabbitmq:3.8.9
